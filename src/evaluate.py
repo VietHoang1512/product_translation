5d2d354045506c70ec88ec0d1f921d3078c129c8
unknown <warkingleo2000@gmail.com>
2020-08-05 09:00:37 +0700

    Submit source code

---
+++ src/evaluate.py
@@ -0,0 +1,28 @@
+import re
+import regex
+from sacrebleu import corpus_bleu
+
+"""Sample evaluation script for product title translation."""
+
+OTHERS_PATTERN = regex.compile(r'\p{So}')
+
+
+def eval(preds: List[str], refs: List[str]) -> float:
+    """BLEU score computation.
+
+    Strips all characters belonging to the unicode category "So".
+    Tokenize with standard WMT "13a" tokenizer.
+    Compute 4-BLEU.
+
+    Args:
+        preds (List[str]): List of translated texts.
+        refs (List[str]): List of target reference texts.
+    """
+    preds = [OTHERS_PATTERN.sub(' ', text) for text in preds]
+    refs = [OTHERS_PATTERN.sub(' ', text) for text in refs]
+    return corpus_bleu(
+        preds, [refs],
+        lowercase=True,
+        tokenize='13a',
+        use_effective_order= False
+    ).score, preds, refs
